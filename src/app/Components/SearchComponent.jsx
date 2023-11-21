1da7ed89ba64d4bbb50665e5a2d02ecb0046cab3
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-11-21 11:58:51 +0100

    Just creating the UI

---
+++ src/app/Components/SearchComponent.jsx
@@ -0,0 +1,142 @@
+'use client'
+import React from 'react'
+import sc from '../Styles/SearchComponent.module.css'; 
+
+function SearchComponent() {
+  return (
+    <div id={sc.mainContainer}>
+    <div id={sc.subContainer} >
+    
+    <div id={sc.componentContainer} >
+    
+    
+    <div id={sc.titleContainer} >
+    <div id={sc.titleSection} >
+    
+    
+    
+    
+    </div>
+    
+
+    
+    
+    </div>
+    
+    
+    {/* Add in the button area here  */}
+    <div id={sc.buttonAreaContainer} >
+    <div id={sc.buttonArea} >
+    
+    <div id={sc.buttonContainer} >
+    
+    
+    {/* Add in the button here  */}
+    <button>
+
+        Find a deal 
+        
+        
+
+    </button>
+    
+    
+    </div>
+    <div id={sc.buttonContainer} >
+    
+    
+    {/* Add in the button here  */}
+    <button>
+
+        Find a deal 
+        
+        
+
+    </button>
+    
+    
+    </div>
+    <div id={sc.buttonContainer} >
+    
+    
+    {/* Add in the button here  */}
+    <button>
+
+        Find a deal 
+        
+        
+
+    </button>
+    
+    
+    </div>
+    <div id={sc.buttonContainer} >
+    
+    
+    {/* Add in the button here  */}
+    <button>
+
+        Find a deal 
+        
+        
+
+    </button>
+    
+    
+    </div>
+    <div id={sc.buttonContainer} >
+    
+    
+    {/* Add in the button here  */}
+    <button>
+
+        Find a deal 
+        
+        
+
+    </button>
+    
+    
+    </div>
+    <div id={sc.buttonContainer} >
+    
+    
+    {/* Add in the button here  */}
+    <button>
+
+        Find a deal 
+        
+
+    </button>
+    
+    
+    </div>
+
+        
+        
+    </div>
+    
+
+    
+    </div>
+    
+
+    </div>
+
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    </div>
+    
+    
+    
+    
+  )
+}
+
+export default SearchComponent
